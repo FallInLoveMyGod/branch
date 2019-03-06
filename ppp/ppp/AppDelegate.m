cffd35abe5dcd9e058a5c6bacdbfeffdf9dcfa1f
Tianyaoqi <1174465110@qq.com>
2019-03-06 16:09:34 +0800

    test

--- ppp/ppp/AppDelegate.m
+++ ppp/ppp/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    // 页面启动窗口
     return YES;
 }
 
